1f2ad0bf810238ddd4bf1080e5005b6e8c2aa7d0
Alberto Conti <albertoconti@gmail.com>
2013-06-26 17:01:36 -0400

    removed jitter

--- zip.R
+++ zip.R
@@ -9,14 +9,9 @@ lon <- zipcode$longitude
 longitudeLimits <- c(-130, -63)
 latitudeLimits <- c(23, 55)
 ggplot(zipcode, aes(lon,lat)) + geom_point() + xlim(longitudeLimits) + ylim(latitudeLimits)
-       
-########## POINTS PLOT ###########
-#add some random jitter
-longitudeJitterSize<-diff(range(longitudeLimits))/5000.
-latitudeJitterSize<-diff(range(latitudeLimits))/5000.
-#longitudeJitters<-runif(nrow(mergedZips), -longitudeJitterSize, longitudeJitterSize)
-#latitudeJitters<-runif(nrow(mergedZips), -latitudeJitterSize, latitudeJitterSize)
 
+
+# Create Nice Looking PNG
 png(filename="pointsmap.png", type="quartz", bg="white", width=10.*960, height=10.*960, pointsize=1)
 map("state", col="#000000", fill=TRUE, bg="#FFFFFF", lwd=1.0, xlim=longitudeLimits, ylim=latitudeLimits)
 points(lon,lat, col="#FA9C58", pch=19, cex=0.01)
